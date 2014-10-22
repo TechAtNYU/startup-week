862fcf377a37aedc638a867a4d81475642d172d3
Cheryl Wu <grungerabbit@gmail.com>
2014-10-21 19:55:38 -0700

    manual prioritizing

--- _scripts/process.js
+++ _scripts/process.js
@@ -8,9 +8,19 @@ var manualData = {
   //the event id is the key
   //priority: 1 - low; 2 - medium; 3 - high
   "544061d56b0287336dfc51d4": {
-    isBusiness: false,
+    isBusiness: true,
     priority: 3
   }, 
+
+  "5441a5ac3f621ea514e8dbfc": {
+    isBusiness: false,
+    priority: 1
+  },
+
+  "544318fbe5ac6ddf5357c444": {
+    isBusiness: false,
+    priority: 1
+  },
 
   "5441a4d43f621ea514e8dbfa": {
     isBusiness: false,
@@ -23,6 +33,91 @@ var manualData = {
   },
 
   "5442f9aa32ff61083f0df839": {
+    isBusiness: false,
+    priority: 3
+  },
+
+  "5443027c32ff61083f0df840": {
+    isBusiness: true,
+    priority: 3
+  },
+
+  "54431910e5ac6ddf5357c445": {
+    isBusiness: false,
+    priority: 1
+  },
+
+  "5443194ce5ac6ddf5357c446": {
+    isBusiness: false,
+    priority: 1
+  },
+
+  "5442fba132ff61083f0df83b": {
+    isBusiness: false,
+    priority: 2
+  },
+
+  "54431bcae5ac6ddf5357c449": {
+    isBusiness: false,
+    priority: 2
+  },
+
+  "544304c6e5ac6ddf5357c428": {
+    isBusiness: false,
+    priority: 3
+  },
+
+  "544307aee5ac6ddf5357c42f": {
+    isBusiness: true,
+    priority: 2
+  },
+
+  "544305e3e5ac6ddf5357c429": {
+    isBusiness: true,
+    priority: 1
+  },
+  "54430c45e5ac6ddf5357c436": {
+    isBusiness: true,
+    priority: 1
+  },
+  "54430d08e5ac6ddf5357c438": {
+    isBusiness: true,
+    priority: 1
+  },
+  "54430d6be5ac6ddf5357c43b": {
+    isBusiness: true,
+    priority: 1
+  },
+  "54430efbe5ac6ddf5357c43c": {
+    isBusiness: true,
+    priority: 1
+  },
+  "54430f4be5ac6ddf5357c43d": {
+    isBusiness: true,
+    priority: 1
+  },
+
+  "5443037c32ff61083f0df841": {
+    isBusiness: false,
+    priority: 2
+  },
+
+  "54431960e5ac6ddf5357c447": {
+    isBusiness: false,
+    priority: 1
+  },
+
+  "54430b21e5ac6ddf5357c431": {
+    isBusiness: false,
+    priority: 2
+  },
+ 
+  "54432632e5ac6ddf5357c44c": {
+    isBusiness: false,
+    priority: 3
+  }, 
+
+  "54431029e5ac6ddf5357c43e": {
     isBusiness: false,
     priority: 3
   }

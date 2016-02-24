14bef0be82832c87db5886ef53c00f5fe516656a
Omayeli Arenyeka <aomayeli@gmail.com>
2016-02-23 22:14:14 -0500

    changing fonts and setting up to draw circles

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,34 @@
  */
 angular.module('startupWeekApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  		var canvas = document.getElementById('circles'),
+    	context = canvas.getContext('2d');
+    	var circles = [];
+
+    	function drawCircles() {
+    		for (var i = 0; i <= 20; i++) {
+    			circles[i] = new Circle(getColor(), Math.random()*10); 
+    		}
+    	}
+    	drawCircles();
+  		function getColor() {
+			var colors = ['#246c78', '#dda6a2', '#c25c6d'];
+			return colors[Math.floor(Math.random() * colors.length)];
+		}
+
+  		function Circle(color, size) {
+  			this.size = size;
+  			this.color = color;
+
+  			this.draw = function() {
+
+  			};
+  			this.move = function() {
+
+  			};
+  			this.reset = function() {
+
+  			};
+  		}
   });

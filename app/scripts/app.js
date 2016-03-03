d3da86287387e806deb4cf675d2e3014d05b4861
Omayeli Arenyeka <aomayeli@gmail.com>
2016-03-02 22:47:05 -0500

    trying out animations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -89,11 +89,16 @@ app.directive('drawing', function(){
 		function drawCircles() {
 		  for (var i = 0; i <= 30; i++) {
 		  	  circles[i] = new Circle(getColor(), Math.random()*5); 
-		      circles[i].draw();
 		  }
-		  for (var j = 0; j <= 30; j++) {
-		      circles[j].draw();
-		  }
+		  draw();
+		}
+		function draw() {
+			context.clearRect(0, 0, element[0].width, element[0].height);
+			 for (var j = 0; j <= 30; j++) {
+			  	  circles[j].draw();
+			      circles[j].move();
+		 	 }
+			requestAnimationFrame(draw);
 		}
 		drawCircles();
 		function getColor() {
@@ -106,6 +111,10 @@ app.directive('drawing', function(){
 			this.y = ((element[0].height-50) * Math.random());
 			this.size = size;
 			this.color = color;
+			this.minSpeed = .05;
+ 			this.maxSpeed = .5;
+ 			this.xVelocity = Math.random(this.minSpeed, this.maxSpeed);
+ 			this.yVelocity = Math.random(this.minSpeed, this.maxSpeed);
 
 			this.draw = function() {
 				context.beginPath();
@@ -114,11 +123,20 @@ app.directive('drawing', function(){
 				context.fillStyle = this.color;
 				context.fill();
 			};
-			this.shake = function() {
-
+			this.move = function() {
+					this.x += this.xVelocity;
+					this.y += this.yVelocity;
+					if (this.x > element[0].width + this.size || this.y > element[0].height + this.size) {
+						this.reset();
+					}
+					else {
+						this.draw();
+					}
 			};
 			this.reset = function() {
-
+				this.x = ((element[0].width-50) * Math.random());
+				this.y = ((element[0].height-50) * Math.random());
+				this.draw();
 			};
 		} 
     }

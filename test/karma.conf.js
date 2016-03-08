46595e77f8b1b9a3b266615e3d7450e6592b5bab
Omayeli Arenyeka <aomayeli@gmail.com>
2016-03-08 08:36:24 -0500

    changing karma configs

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,9 +32,9 @@ module.exports = function(config) {
       'bower_components/underscore/underscore.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      "app/scripts/*.js",
+      "test/mock/*.js",
+      "test/spec/*.js"
     ],
 
     // list of files / patterns to exclude

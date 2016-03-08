ed4460864124e26d4a8fe30f08c66e9b0bd571e4
Omayeli Arenyeka <aomayeli@gmail.com>
2016-03-08 08:11:27 -0500

    adding links

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # startup-week
 
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
-version 0.14.0.
+version 0.11.1.
 
 ## Build & development
 
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,35 +1,36 @@
 // Karma configuration
-// http://karma-runner.github.io/0.12/config/configuration-file.html
-// Generated on 2016-02-20 using
-// generator-karma 1.0.1
+// Generated on 2016-03-07
 
 module.exports = function(config) {
-  "use strict";
+  'use strict';
 
   config.set({
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
     // base path, that will be used to resolve files and exclude
-    basePath: "../",
+    basePath: '../',
 
     // testing framework to use (jasmine/mocha/qunit/...)
     // as well as any additional frameworks (requirejs/chai/sinon/...)
     frameworks: [
-      "jasmine"
+      'jasmine'
     ],
 
     // list of files / patterns to load in the browser
     files: [
       // bower:js
       'bower_components/angular/angular.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      "app/scripts/*.js",
-      "test/mock/*.js",
-      "test/spec/*.js"
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
@@ -48,13 +49,13 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: [
-      "PhantomJS"
+      'PhantomJS'
     ],
 
     // Which plugins to enable
     plugins: [
-      "karma-phantomjs-launcher",
-      "karma-jasmine"
+      'karma-phantomjs-launcher',
+      'karma-jasmine'
     ],
 
     // Continuous Integration mode
@@ -67,11 +68,11 @@ module.exports = function(config) {
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    // Uncomment the following lines if you are using grunt"s server to run the tests
+    // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
-    //   "/": "http://localhost:9000/"
+    //   '/': 'http://localhost:9000/'
     // },
     // URL root prevent conflicts with the site root
-    // urlRoot: "_karma_"
+    // urlRoot: '_karma_'
   });
 };
